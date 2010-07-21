73aed04ddd3c894960178d99e662733a977c1c29
Gennady Kushnir <genkush@rujel.net>
2010-07-21 13:53:28 +0400

    возможность хранения пользователей в базе данных с указанием, каким учителем какой пользователь является изменения в структуре БД: для хранения пользователей

--- SQL/MySQL/rujelMySQLuser.sql
+++ SQL/MySQL/rujelMySQLuser.sql
@@ -2,3 +2,4 @@ GRANT ALL PRIVILEGES ON `RujelStatic`.* TO 'rujel'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON `RujelYear%`.* TO 'rujel'@'localhost';
 GRANT ALL PRIVILEGES ON `Contacts`.* TO 'rujel'@'localhost';
 GRANT ALL PRIVILEGES ON `VseLists`.* TO 'rujel'@'localhost';
+GRANT ALL PRIVILEGES ON `RujelUsers`.* TO 'rujel'@'localhost';
