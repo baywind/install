1e17ce0a78178b9ca6e03000bd51532129c61b58
Gennady Kushnir <genkush@rujel.net>
2012-01-25 00:33:38 +0400

    улучшения в интерфейса расписаний. номер урока. предположение следующего урока по расписанию.

--- Frameworks/RujelSchedule.framework/WebServerResources/schedule.js
+++ Frameworks/RujelSchedule.framework/WebServerResources/schedule.js
@@ -1,17 +1,32 @@
 
 function schedCell(cell) {
 	var tick = cell.getElementsByTagName("input")[0];
+	var span = cell.getElementsByTagName("span");
+	if(span) {
+		for(var i=0; i<span.length; i++) {
+			if(span[i].className == "currCrs") {
+				span = span[i];
+				break;
+			}
+		}
+		if(!span.className)
+			span = false;
+	}
 	tick.checked = !tick.checked;
 	unDim(cell);
 	if(tick.checked) {
 		tick.className = cell.className;
 		cell.className = "selection";
+		if(span)
+			span.style.color = "#000000";
 	} else {
 		if(tick.className)
 			cell.className = tick.className
 		else
 			cell.className = "grey";
 		tick.className = null;
+		if(span)
+			span.style.color = "#666666";
 	}
 	dim(cell);
 }
